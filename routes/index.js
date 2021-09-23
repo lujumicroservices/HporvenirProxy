fc60876d0dd1103f18db6709742a693976362815
lujumicroservices <lujumicroservices@gmail.com>
2021-09-22 19:28:42 -0500

    get ip

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req)
-  res.render('index', { title: req.socket.remoteAddress });
+  var ip = requestIp.getClientIp(req);
+  res.render('index', { title: ip });
 });
 
 module.exports = router;
+
+
+http://remoto.dgb.uanl.mx/login?url=http://hporvenirproxy.azurewebsites.net/
+
