22fbd94c3c05dad34cea5db893654a59be6f1ab3
lujumicroservices <lujumicroservices@gmail.com>
2021-09-27 14:37:34 -0500

    remove redirect

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
   var json2 = JSON.stringify(req.rawHeaders);
   var ip = 1;
 
-  res.redirect('http://hemerotecaelporvenir.com.mx/');
+  //res.redirect('http://hemerotecaelporvenir.com.mx/');
 
   console.log(req);
   res.render('index', { title: ip , json : json, raw : json2 });
