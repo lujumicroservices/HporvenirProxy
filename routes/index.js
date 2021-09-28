c0614247925439f9ba2b0e0633f71ba95c0724f0
lujumicroservices <lujumicroservices@gmail.com>
2021-09-27 21:31:12 -0500

    pass secret

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 var express = require('express');
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-ctr';
+const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
+const iv = crypto.randomBytes(16);
 
 var router = express.Router();
 
@@ -7,13 +12,41 @@ router.get('/', function(req, res, next) {
   
   var json = JSON.stringify(req.headers);
   var json2 = JSON.stringify(req.rawHeaders);
-  var ip = 1;
+  
+  let ts = Date.now();
+  var ip = req.headers["x-client-ip"];
+
+  const enc1 = encrypt(ip + "@" + ts);
+  
 
   //res.redirect('http://hemerotecaelporvenir.com.mx/');
-
-  console.log(req);
-  res.render('index', { title: ip , json : json, raw : json2 });
+  res.redirect('http://hemerotecaelporvenir.com.mx/?secret=' + enc1.content + "&iv=" + enc1.iv);
+     
+    
+  
+  res.render('index', { title: ip , json : json, raw : json2, enc : JSON.stringify(enc1) });
 });
+
+const encrypt = (text) => {
+  
+  const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+
+  const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+
+  return {
+      iv: iv.toString('hex'),
+      content: encrypted.toString('hex')
+  };
+};
+
+const decrypt = (hash) => {
+
+  const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(hash.iv, 'hex'));
+
+  const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash.content, 'hex')), decipher.final()]);
+
+  return decrpyted.toString();
+};
 
 module.exports = router;
 
