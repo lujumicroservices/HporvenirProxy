74fda1fa68c7bf9ed1ca343c3cc243f566f0d560
LuJu Microservices <49590535+lujumicroservices@users.noreply.github.com>
2025-02-04 22:30:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
